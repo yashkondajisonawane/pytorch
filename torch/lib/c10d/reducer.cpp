519afc8e04f66751fda5644fbada3c63fe38d44b
yanlizhao <yanlizhao@fb.com>
2021-01-29 14:39:27 -0800

    Create PyTorch DDP logging APIs for applications to use
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/50637
    
    add APIs for logging pytorch ddp logging data in applications.
    
    Differential Revision: [D25933411](https://our.internmc.facebook.com/intern/diff/D25933411/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D25933411/)!
    ghstack-source-id: 120692215

--- torch/lib/c10d/reducer.cpp
+++ torch/lib/c10d/reducer.cpp
@@ -1468,8 +1468,8 @@ void Reducer::ensure_prior_reduction_finished() {
 
 void Reducer::set_construction_logging_data(
   const std::string& module_name,
-  const std::vector<int>& device_ids,
-  int output_device,
+  const std::string& device_ids,
+  const std::string& output_device,
   bool broadcast_buffers
 ) {
   ddp_logging_data_->module_name = module_name;
@@ -1482,6 +1482,8 @@ void Reducer::set_construction_logging_data(
   ddp_logging_data_->find_unused_parameters = find_unused_parameters_;
   ddp_logging_data_->gradient_as_bucket_view = gradient_as_bucket_view_;
   ddp_logging_data_->backend_name = process_group_->getBackendName();
+
+  LogPyTorchDDPUsage(*ddp_logging_data_);
 }
 
 c10::DDPLoggingData Reducer::get_ddp_logging_data() {
