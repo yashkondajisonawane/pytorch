5c251a25192151c7b2c7f821dbed7abb5de82938
yanlizhao <yanlizhao@fb.com>
2021-02-01 18:21:09 -0800

    add stats that can only be collected at runtime
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/51386
    
    add stats such as rebuilt bucket stats, unused parameter stats and performance stats to ddp logging data
    ghstack-source-id: 120818571
    
    Differential Revision: [D26158645](https://our.internmc.facebook.com/intern/diff/D26158645/)

--- c10/util/Logging.h
+++ c10/util/Logging.h
@@ -347,7 +347,21 @@ struct DDPLoggingData {
   bool find_unused_parameters = false;
   bool gradient_as_bucket_view = false;
 
-  // Runtime states
+  // Runtime stats are collected during training loop.
+  // Users can get these stats at any iteration of training
+  // loop by calling get_ddp_logging_data() in python.
+
+  // Total unused parameter size (Bytes)
+  // It is calculated after forward computation in each iteration.
+  int unused_parameter_size = 0;
+  // Rebuild buckets stats after 1st iteration
+  bool has_rebuilt_buckets = false;
+  std::vector<int> rebuilt_bucket_sizes;
+  // Average performance stats per iteration (ns)
+  int avg_forward_compute_time = 0;
+  int avg_backward_compute_time = 0;
+  int avg_backward_comm_time = 0;
+  int avg_backward_compute_comm_overlap_time = 0;
 };
 
 C10_API void SetPyTorchDDPUsageLogger(std::function<void(const c10::DDPLoggingData&)> logger);
